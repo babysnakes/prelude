1729d0f1c994bbad0f163de2d5d433e6aeee4cf7
Haim Ashkenazi <haim@babysnakes.org>
2012-06-09 18:07:27 +0300

    auto-complete (enabled and ignores).

--- personal/stuff.el
+++ personal/stuff.el
@@ -1,3 +1,8 @@
+;; auto-complete:
+;; somehow it doesn't get activated by the package.
+(require 'auto-complete-config)
+(ac-config-default)
+
 ;; chef-mode
 (add-to-list 'load-path (concat prelude-vendor-dir "chef-mode"))
 (require 'chef-mode)
